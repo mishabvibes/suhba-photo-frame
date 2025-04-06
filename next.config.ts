482647815db50fd3e3d3cc58c2daf6b584995a4c
mishabvibes <mdmishab124@gmail.com>
2025-04-06 19:08:41 +0530

    first commit

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  typescript: {
+    ignoreBuildErrors: true, // Ignores TypeScript errors during build
+  },
+  eslint: {
+    ignoreDuringBuilds: true, // Ignores ESLint errors during build
+  },
+  images: {
+    domains: ['tpgtrwvdzjvfkhjotzex.supabase.co'],
+  },
 };
 
 export default nextConfig;
